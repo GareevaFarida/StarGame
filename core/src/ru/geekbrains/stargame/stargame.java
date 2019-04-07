90532c3ed9c39e0d91d604de5bf353fb86c40e21
GareevaFarida <gareeva.farida@gmail.com>
2019-04-08 00:02:15 +0300

    Практическое задание 1

---
+++ core/src/ru/geekbrains/stargame/stargame.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.stargame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class stargame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	Texture imgFone;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		imgFone = new Texture("fone4.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(imgFone,0,0,1920,1080);
+		batch.draw(img, 50, 50);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+		imgFone.dispose();
+	}
+}
