2861d46d6578ec155e0c4f8b50f64f17dbeabf51
GareevaFarida <gareeva.farida@gmail.com>
2019-04-11 11:36:38 +0300

    Практическое задание 2. Выполнено дополнительное задание со звездочкой, исправлена ошибка в основном задании: 1. Реализовано движение логотипа badlogic (можно свою картинку вставить) при нажатии клавиши мыши (touchDown) в точку нажатия на экране и остановку в данной точке. 2. Реализовано управление логотипом с помощью клавиш со стрелками на клавиатуре*** (дополнительное задание, не обязательное к выполнению)

--- core/src/screen/MenuScreen.java
+++ core/src/screen/MenuScreen.java
@@ -13,22 +13,29 @@ public class MenuScreen extends BaseScreen {
     private Vector2 v;
     private Texture img;
     private Vector2 direction;
+    private Vector2 pointOfDestination;
     private float speed;
     private int imgWidth;
     private int imgHeight;
+    private static final int SCREEN_HIGHT = Gdx.graphics.getHeight();
+    private static final int SCREEN_WIDTH = Gdx.graphics.getWidth();
+    private static final int UP = 19;
+    private static final int DOWN = 20;
+    private static final int LEFT = 21;
+    private static final int RIGHT = 22;
 
     @Override
     public void show() {
         super.show();
-        touch       = new Vector2();
-        pos         = new Vector2();
-        direction   = new Vector2();
-        v           = new Vector2(0.2f, 0.5f);
-
-        speed       = v.len();
-        img         = new Texture("badlogic.jpg");
-        imgWidth    = img.getWidth();
-        imgHeight   = img.getHeight();
+        touch = new Vector2();
+        pos = new Vector2();
+        direction = new Vector2();
+        pointOfDestination = new Vector2();
+        v = new Vector2(1, 1);
+        speed = v.len();
+        img = new Texture("badlogic.jpg");
+        imgWidth = img.getWidth();
+        imgHeight = img.getHeight();
     }
 
     @Override
@@ -38,11 +45,8 @@ public class MenuScreen extends BaseScreen {
         batch.begin();
         batch.draw(img, pos.x, pos.y);
         batch.end();
-        if (Math.abs(pos.x - touch.x) < speed ||
-                pos.x <= 0 ||
-                pos.x > Gdx.graphics.getWidth() - imgWidth ||
-                pos.y <= 0 ||
-                pos.y > Gdx.graphics.getHeight() - imgHeight
+        if (Math.abs(pos.x - pointOfDestination.x) < speed
+                & Math.abs(pos.y-pointOfDestination.y)< speed
         ) v.setZero();
     }
 
@@ -54,13 +58,51 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        touch.set(screenX, SCREEN_HIGHT - screenY);
         System.out.println("touchDown touch.x = " + touch.x + " touch.y = " + touch.y);
-        direction.set(touch.cpy().sub(pos));
+        pointOfDestination.set(touch);
+        changeDirection();
+
+        return false;
+    }
+
+    private void changeDirection() {
+        direction.set(pointOfDestination.cpy().sub(pos));
         direction.nor();
         direction.scl(speed);
         v.set(direction);
+    }
 
-        return false;
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case UP: {
+                pointOfDestination.set(pos.x, SCREEN_HIGHT);
+                changeDirection();
+                break;
+            }
+            case DOWN: {
+                pointOfDestination.set(pos.x, 0);
+                changeDirection();
+                break;
+            }
+            case LEFT: {
+                pointOfDestination.set(0, pos.y);
+                changeDirection();
+                break;
+            }
+            case RIGHT: {
+                pointOfDestination.set(SCREEN_WIDTH, pos.y);
+                changeDirection();
+                break;
+            }
+        }
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        v.setZero();
+        return super.keyUp(keycode);
     }
 }
